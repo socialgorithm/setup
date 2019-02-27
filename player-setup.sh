f817f0eec74b591c95b4bc635e560511e410e2bb
Bharat Reddy <me@bharatreddy.com>
2019-02-27 17:45:43 +0000

    Initial commit

---
+++ player-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Welcome to your Socialgorithm workshop!"
+
+command -v npm >/dev/null 2>&1 || { echo >&2 "I require npm but it's not installed. Aborting."; exit 1; }
+command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed. Aborting."; exit 1; }
+
+# Download UABC
+command -v uabc >/dev/null 2>&1 || npm install -g @socialgorithm/uabc
+
+# Ask which games
+read -p "What game are you competing in today?: " GAME
+git clone git@github.com:socialgorithm/${GAME}-starter
